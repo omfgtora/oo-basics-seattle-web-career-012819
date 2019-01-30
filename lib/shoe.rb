6f57219918a757b4a133bdc04560c26c3b4ddcdf
Ethan Roberts <ejrobe@gmail.com>
2019-01-30 01:16:38 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 class Shoe
-  attr_reader :brand, :color, :size, :material
-  attr_accessor :condition
+  attr_reader :brand
+  attr_accessor :condition, :color, :size, :material
   def initialize(brand)
     @brand = brand
     @color
